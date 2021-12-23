dfae49bf42125fc165223a74a297d73f83df6b26
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-23 16:27:41 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
+console.log(player)
 let progress = $('.progress__filled').first();
+console.log(progress)
+
+
+$(function(){
+    $('.playerbutton').click(function (e){
+        
+
+    })
+});
