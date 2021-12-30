b849e3f21a2c57c7f4aa2653703dd4ca3a18494d
Olivier02500 <olivier.peuchet@hotmail.com>
2021-12-30 10:53:02 +0100

    make exo 124

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
-console.log(player)
 let progress = $('.progress__filled').first();
-console.log(progress)
+let play = $('.player__button');
+let x = true;
+
+play.click(function (){
+    if(x === true){
+        $(this).html( "►");
+        player.get(0).play();
+        x = false;
+    }
+    else if(x === false) {
+        $(this).html("⏸");
+        player.get(0).pause();
+        x = true;
+    }
+})
 
 
-$(function(){
-    $('.playerbutton').click(function (e){
-        
+player.click("timeupdate", () => {
+    let progre = player.currentTime / player.duration;
+    progress.style.width = progre * 100 + "%";
+});
 
-    })
+$("button").click(function (){
+    switch ($(this).attr("data-skip")){
+        case "-10":
+            player[0].currentTime -= 10;
+            $(this).html("« 10s");
+            break;
+        case "25":
+            player[0].currentTime += 25;
+            $(this).html("25s »");
+            break;
+    }
 });
